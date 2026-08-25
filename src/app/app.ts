d06f975cba4025637ea08ec89a3c867c70845f91
Pradyumna Rajbhandari <prajbhandari@infinite.com>
2026-08-25 17:14:51 +0545

    Day 1

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
 })
 export class App {
   protected readonly title = signal('hrm');
+  header: string = "Second Line";
 }
