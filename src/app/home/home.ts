74ac847eddb89cc4228fcbc861e375159aa86cd2
Pradyumna Rajbhandari <prajbhandari@infinite.com>
2026-09-03 17:17:06 +0545

    Form types

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,27 +1,39 @@
-import { AfterViewInit, Component, effect, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
-import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
-import { HomeService } from './services/home.service';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule, ReactiveFormsModule],
   selector: 'app-home',
   styleUrl: './home.scss',
   templateUrl: './home.html',
 })
-export class Home implements OnInit {
-  private homeService = inject(HomeService);
-  total: number = 0;
+export class Home {
+  username: string = '';
+  password: string = '';
+  
+  userReactiveForm: FormGroup = new FormGroup({
+    username: new FormControl<string>('', [Validators.required, Validators.minLength(5)]),
+    password: new FormControl<string>('', [Validators.required, Validators.minLength(8)]),
+    confirmPassword: new FormControl<string>('')
+  });
 
-  ngOnInit(): void {
-    let x = 5;
-    let y = 14;
-    this.total = this.homeService.sum(x, y);
-
-    console.log(this.homeService.getDataFromLoginPage());
+  onSubmit(): void{
+    //do anything with your logic
+    console.log(this.username);
+    console.log(this.password)
   }
 
-  // ngAfterViewInit(): void {
-  //   this.message = 'Rendered';
-  // }
+  onSubmitTemplate(formData: NgForm): void{
+    console.log(formData);
+  }
 
+  onSubmitReactive(): void{
+    if(this.userReactiveForm.invalid){
+      console.log("Invalid");
+      return;
+    }
+    console.log(this.userReactiveForm.value)
+  }
 }
